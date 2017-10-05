c742b21128c0fa5d234d1d17950aa92fb1f6730a
Daishi Kaszer <daishi.kaszer@yahoo.fr>
2017-10-05 12:17:24 +0200

    Mise à jour du code

--- js/template.js
+++ js/template.js
@@ -1,5 +1,9 @@
+var data = {
+  name: 'Toto'
+};
+
 var hello = document.querySelector('#hello');
 var helloTemplate = document.querySelector('#hello-template').innerHTML;
 var compiledHelloTemplate = _.template(helloTemplate);
-var html = compiledHelloTemplate({name: 'Toto'});
+var html = compiledHelloTemplate(data);
 hello.innerHTML = html;
